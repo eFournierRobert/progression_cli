9f5e6dc7ad9436c17448143080de74bba974264e
efournierrobert <elliott1447@gmail.com>
2024-03-30 23:55:22 -0400

    Documenté le code

--- src/utilisateur/mod.rs
+++ src/utilisateur/mod.rs
@@ -7,6 +7,14 @@ use std::{fs, io};
 use requetes::get_token;
 use rpassword::read_password;
 
+/// Ceci est une fonction publique qui récupère les identifiants de l'utilisateur et 
+/// qui fait les étapes nécessaires à la connexion à Progression.
+/// 
+/// ## Cas d'utilisation
+/// Commence toute les étape nécessaire à la connexion à Progression.
+/// 
+/// ## Retourne 
+/// Unit
 pub fn connexion() {
     let mut username = String::new();
 
@@ -25,12 +33,44 @@ pub fn connexion() {
     stocker_token(token.as_str());
 }
 
+/// Ceci est une fonction publique qui supprime le contenu du fichier contenant le token et le nom d'utilisateur.
+/// 
+/// ## Cas d'utilisation
+/// Pour supprimer le token de l'utilisateur, donc le "déconnecter" de Progression.
+/// 
+/// ## Retourne 
+/// Unit
 pub fn déconnexion() {
     _ = fs::write("./progressioncli/token", "");
 
     println!("Déconnexion réussi");
 }
 
+/// Ceci est une fonction qui permet de bien formatter et d'encoder le nom d'utilisateur,
+/// le mot de passe et le domaine en base64.
+/// 
+/// ## Cas d'utilisation
+/// Pour faire le header d'autorisation "Basic" qui est requis pour l'authentification initiale à Progression.
+/// 
+/// ## Paramètres
+/// - username - Le nom d'utilisateur de l'usager.
+/// - mot_de_passe - Le mot de passe de l'usager.
+/// 
+/// ## Retourne 
+/// Nom d'utilisateur, mot de passe et domaine en base64.
+/// 
+/// ## Exemple
+/// ```
+/// let username = "username";
+/// let mot_de_passe = "mot_de_passe";
+/// 
+/// let credentials_encodés = préparation_credentials(username, mot_de_passe);
+/// 
+/// assert_eq!(
+///     "dXNlcm5hbWU6bW90X2RlX3Bhc3NlOmR0aS5jcm9zZW1vbnQucXVlYmVj",
+///     credentials_encodé
+/// );
+/// ```
 fn préparation_credentials(mut username: String, mot_de_passe: String) -> String {
     let len_username = username.len();
     
@@ -47,6 +87,21 @@ fn préparation_credentials(mut username: String, mot_de_passe: String) -> Strin
     return credentials_encodés;
 }
 
+/// Ceci est une fonction qui permet de stocker le token JWT et le nom d'utilisateur dans un fichier.
+/// 
+/// ## Cas d'utilisation
+/// Pour garder en mémoire le token JWT et le nom d'utilisateur de l'usager qui se connecte.
+/// **Token JWT est nécessaire à toute les requêtes**
+/// 
+/// ## Paramètres 
+/// - token - Le token et le nom d'utilisateur à stocker dans un fichier.
+/// 
+/// ## Panics
+/// Panic lorsqu'il n'arrive pas à créer le dossier qui doit contenir le fichier contenant le token JWT.
+/// Message d'erreur : Erreur: Impossible d'enregistrer le token d'authentification \n [erreur reçu lors de la création du dossier]
+/// 
+/// ## Retourne 
+/// Unit
 fn stocker_token(token: &str) {
     let path = "./progressioncli";
     let data_path = std::path::Path::new(path);
--- src/utilisateur/requetes.rs
+++ src/utilisateur/requetes.rs
@@ -3,6 +3,32 @@ use super::deserialize;
 use deserialize::deserialize_token;
 use reqwest::blocking::Client;
 
+/// Cette fonction publique permet de faire la requête HTTPS vers l'API de Pogression pour récupérer un token.
+/// 
+/// ## Cas d'utilisation
+/// À faire pour récupérer un token de l'API de Progression. Nécessaire pour la connexion.
+/// 
+/// ## Paramètres
+/// - credentials - Credentials en base64. Nécessaire pour le header d'autorisation "Basic".
+/// - username - Le nom d'utilisateur de l'utilisateur qui veut se connecter. Nécessaire pour la requête HTTPS.
+/// 
+/// ## Panic
+/// Panic s'il rencontre une erreur lorsqu'il fait la requête HTTPS au serveur de Progression.
+/// 
+/// Message d'erreur: Erreur: JWT token request: [erreur reçu de la requête HTTPS]
+/// 
+/// ## Retourne
+/// Une chaine de charactère avec le token et le nom d'utilisateur.
+/// 
+/// ## Exemple
+/// ```
+/// let credentials = String::from("dXNlcm5hbWU6bW90X2RlX3Bhc3NlOmR0aS5jcm9zZW1vbnQucXVlYmVj");
+/// let username = String::from("username"); 
+/// 
+/// let token = get_token(credentials, username);
+/// 
+/// assert_eq!("jwt\nusername", token);
+/// ```
 pub fn get_token(credentials: String, username: String) -> String {
     let mut url = "https://progression.dti.crosemont.quebec/api/v1/user/".to_string();
     url.push_str(&username);
