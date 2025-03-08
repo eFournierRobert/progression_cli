863dc02c26e7457699c127e28814d2c64d868400
efournierrobert <elliott1447@gmail.com>
2025-03-08 15:18:43 -0500

    Added some doc

--- src/request/deserialize.rs
+++ src/request/deserialize.rs
@@ -1,6 +1,12 @@
 use crate::structs::question::Question;
 use serde_json::Error;
 
+/// Takes a JSON ```String``` for a question and deserialize it.
+/// 
+/// This function takes a JSON ```String``` for a question and deserialize it
+/// into a ```Question``` ```Struct```. It then stores it inside a ```Result```.
+/// 
+/// ```debugging``` will do a debug print if ```true```
 pub fn deserialize_question(json: String, debugging: &bool) -> Result<Question, Error> {
     let question = serde_json::from_str(json.as_str());
 
