0e148017430fee9326b18671250ade5871bb8966
efournierrobert <elliott1447@gmail.com>
2025-03-09 19:31:32 -0400

    Added some doc

--- src/structs/submit.rs
+++ src/structs/submit.rs
@@ -1,10 +1,17 @@
 use serde::Serialize;
 
+/// SubmitBody ```struct```.
+/// 
+/// This ```struct``` is the container for all the data 
+/// necessary for a answer POST request.
 #[derive(Serialize)]
 pub struct SubmitBody {
     pub data: Data
 }
 
+/// Data ```struct```.
+/// 
+/// This ```struct``` contains some data regarding a submit POST.
 #[derive(Serialize)]
 pub struct Data {
     #[serde(rename="type")]
@@ -12,6 +19,10 @@ pub struct Data {
     pub attributes: Attributes,
 }
 
+/// Attributes ```struct```.
+/// 
+/// This ```struct``` contains the necessary attributes of 
+/// a submit POST.
 #[derive(Serialize)]
 pub struct Attributes {
     pub langage: String,
--- src/structs/submit_response.rs
+++ src/structs/submit_response.rs
@@ -1,16 +1,31 @@
 use serde::Deserialize;
 
+/// SubmitResponse ```struct```.
+/// 
+/// This ```struct``` is the container for all the data received
+/// from a submit POST.
 #[derive(Deserialize, Debug)]
 pub struct SubmitResponse {
     pub data: Data,
     pub included: Vec<Included>
 }
 
+/// /// Data ```struct```.
+/// 
+/// This ```struct``` contains some data regarding a 
+/// submit response.
 #[derive(Deserialize, Debug)]
 pub struct Data {
     pub attributes: Attributes
 }
 
+/// Attributes ```struct```.
+/// 
+/// This ```struct``` contains all the attributes received
+/// after a submit POST. **Note that feedback, réussi and tests_réussis
+/// are all under SubmitResponse.data.attributes and résultat, 
+/// sortie_erreur, sortie_observée and temps_exécution are all under
+/// SubmitResponse.included.attributes**.
 #[derive(Deserialize, Debug)]
 pub struct Attributes {
     pub feedback: Option<String>,
@@ -22,6 +37,14 @@ pub struct Attributes {
     pub temps_exécution: Option<u16>
 }
 
+/// Included ```struct```.
+/// 
+/// This ```struct``` is the container for all the data received
+/// after a submit POST.
+/// 
+/// Only used to get the résultat, sortie_erreur, sortie_observée and 
+/// temps_exécution in the JSON returned from the API.
+/// **All the other attributes are store inside ```Data```**.
 #[derive(Deserialize, Debug)]
 pub struct Included {
     pub attributes: Attributes
