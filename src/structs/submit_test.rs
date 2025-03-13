9680470e8f16daacb30ca7d9bb4802419d54260a
efournierrobert <elliott1447@gmail.com>
2025-03-12 21:14:42 -0400

    Forgot to doc the structs

--- src/structs/submit_test.rs
+++ src/structs/submit_test.rs
@@ -1,10 +1,17 @@
 use serde::Serialize;
 
+/// SubmitTest ```struct```.
+/// 
+/// This ```struct``` is the container for all the 
+/// necessary data for a test POST request.
 #[derive(Serialize)]
 pub struct SubmitTest {
     pub data: Data,
 }
 
+/// Data ```struct```.
+/// 
+/// This ```struct``` containts some data regarding a test POST.
 #[derive(Serialize)]
 pub struct Data {
     #[serde(rename = "type")]
@@ -12,6 +19,10 @@ pub struct Data {
     pub attributes: Attributes,
 }
 
+/// Attributes ```struct```.
+/// 
+/// This ```struct``` contains the necessary attributes of 
+/// a test POST.
 #[derive(Serialize)]
 pub struct Attributes {
     pub langage: String,
--- src/structs/submit_test_response.rs
+++ src/structs/submit_test_response.rs
@@ -1,14 +1,26 @@
 use serde::Deserialize;
 
+/// SubmitTestResponse ```struct```.
+/// 
+/// This ```struct``` is the container for the data
+/// received in response of a test POST.
 #[derive(Deserialize)]
 pub struct SubmitTestResponse {
     pub data: Data,
 }
 
+/// Data ```struct```.
+/// 
+/// This ```struct``` contains all the data received after a test POST.
 #[derive(Deserialize)]
 pub struct Data {
     pub attributes: Attributes,
 }
+
+/// Attributes ```struct```.
+/// 
+/// This ```struct``` contains all the necessary attributes
+/// received from the API after a test POST.
 #[derive(Deserialize)]
 pub struct Attributes {
     pub sortie_observée: Option<String>,
